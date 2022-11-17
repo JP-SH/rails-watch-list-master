bcfd7956da2711e5622be7c08c55e5ff29d58663
Jasdeep Singh <j.singhhh917@gmail.com>
2022-11-17 16:49:25 +1100

    server

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -15,7 +15,7 @@ class ListsController < ApplicationController
   def create
     @list = List.new(list_params)
     if @list.save
-      redirect_to @list, notice: "list was successfully created."
+      redirect_to list_path(@list), notice: "list was successfully created."
     else
       render :new, status: :unprocessable_entity
     end
